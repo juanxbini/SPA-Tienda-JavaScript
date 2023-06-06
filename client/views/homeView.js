3d95372bdaaf8d4d0410e84b9272e0e6f034bb05
juanxbini <juanxbini@gmail.com>
2023-06-06 18:03:12 -0300

    Vista home terminada

--- client/views/homeView.js
+++ client/views/homeView.js
@@ -1,31 +1,30 @@
 
 export class HomeView {
-
   render(homeTemplateText, latestArticleTemplateText, promotionalProductsTemplateText, latestArticles, promotionalProducts) {
     // Compilar los templates
-    // Compilar los templates
-    console.log('FUNCION RENDER')
     const homeTemplate = Handlebars.compile(homeTemplateText);
     const latestArticleTemplate = Handlebars.compile(latestArticleTemplateText);
     const promotionalProductsTemplate = Handlebars.compile(promotionalProductsTemplateText);
 
-    // Renderizar los datos en los templates
-    const latestArticleHtml = latestArticleTemplate({ articles: latestArticles });
-    const promotionalProductsHtml = promotionalProductsTemplate({ products: promotionalProducts });
-    console.log('home t', homeTemplate)
-    console.log('latest t', latestArticleTemplate)
-    console.log(promotionalProductsTemplate)
-    // Renderizar la sección Home
-    const homeHtml = homeTemplate({
-      sections: [
-        { sectionTemplate: latestArticleHtml },
-        { sectionTemplate: promotionalProductsHtml }
-      ]
-    });
-    console.log('homehtml', homeHtml)
-    // Mostrar el contenido renderizado en el contenedor correspondiente
+    // Registrar los partials
+    Handlebars.registerPartial('latestArticle', latestArticleTemplate);
+    Handlebars.registerPartial('promotionalProducts', promotionalProductsTemplate);
+
     const contentContainer = document.getElementById('content');
-    console.log(contentContainer)
-    contentContainer.innerHTML = homeHtml;
+
+    // Esperar a que ambas promesas se resuelvan
+    Promise.all([latestArticles, promotionalProducts]).then(([latestArticlesData, promotionalProductsData]) => {
+      // Renderizar los datos en las plantillas
+      const latestArticleHtml = latestArticleTemplate({ articles: latestArticlesData });
+      const promotionalProductsHtml = promotionalProductsTemplate({ products: promotionalProductsData });
+
+      // Renderizar la sección Home
+      const homeHtml = homeTemplate();
+
+      // Mostrar el contenido renderizado en el contenedor correspondiente
+      contentContainer.innerHTML = homeHtml;
+      contentContainer.querySelector('.latest-articles').innerHTML = latestArticleHtml;
+      contentContainer.querySelector('.promotional-products').innerHTML = promotionalProductsHtml;
+    });
   }
 }

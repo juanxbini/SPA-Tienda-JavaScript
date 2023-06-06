bc55f5c452bb6983b7608328ecee43efb686b004
juanxbini <juanxbini@gmail.com>
2023-06-06 02:04:38 -0300

    Correccion esperanzadora para corregir el error

--- client/views/homeView.js
+++ client/views/homeView.js
@@ -3,23 +3,27 @@ export class HomeView {
 
   render(homeTemplateText, latestArticleTemplateText, promotionalProductsTemplateText, latestArticles, promotionalProducts) {
     // Compilar los templates
-    console.log('FUNCION RENDER')
-    const homeTemplate = Handlebars.compile(homeTemplateText);
-    const latestArticleTemplate = Handlebars.compile(latestArticleTemplateText);
-    const promotionalProductsTemplate = Handlebars.compile(promotionalProductsTemplateText);
-    
-    // Renderizar los datos en los templates
-    const latestArticleHtml = latestArticleTemplate({ articles: latestArticles });
-    const promotionalProductsHtml = promotionalProductsTemplate({ products: promotionalProducts });
-
-    // Renderizar la sección Home
-    const homeHtml = homeTemplate({
-      sections: [
-        { sectionTemplate: latestArticleHtml },
-        { sectionTemplate: promotionalProductsHtml }
-      ]
-    });
-
+   // Compilar los templates
+   console.log('FUNCION RENDER')
+   const homeTemplate = Handlebars.compile(homeTemplateText);
+   const latestArticleTemplate = Handlebars.compile(latestArticleTemplateText);
+   const promotionalProductsTemplate = Handlebars.compile(promotionalProductsTemplateText);
+   
+   // Renderizar los datos en los templates
+   const latestArticleHtml = latestArticleTemplate({ articles: latestArticles });
+   const promotionalProductsHtml = promotionalProductsTemplate({ products: promotionalProducts });
+   console.log('home t',homeTemplate)
+   console.log('latest t',latestArticleTemplate)
+   console.log(promotionalProductsTemplate)
+   // Renderizar la sección Home
+   const homeHtml = homeTemplate({
+     sections: [
+       { sectionTemplate: latestArticleHtml },
+       { sectionTemplate: promotionalProductsHtml }
+     ]
+   });
+   console.log('homehtml', homeHtml)
+   // Mostrar el contenido renderizado en el contenedor correspondiente
     // Mostrar el contenido renderizado en el contenedor correspondiente
     const contentContainer = document.getElementById('content');
     contentContainer.innerHTML = homeHtml;

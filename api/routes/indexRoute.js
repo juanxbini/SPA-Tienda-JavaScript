62bfec265ff5b339611513b30495de30f914a051
juanxbini <132483554+juanxbini@users.noreply.github.com>
2023-06-07 17:28:06 -0300

    Delete api directory

--- api/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import productRoutes from './ProductRoute.js';
-
-const router = express.Router();
-
-// Rutas
-router.use('/product', productRoutes);
-
-export default router;
